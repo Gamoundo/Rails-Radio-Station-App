532a5e8fe10644faab8a4ae6eb20d55268f71fa3
Gamoundo <akilgg@gmail.com>
2020-06-13 18:10:09 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  get 'songs/index'
-  get 'songs/show'
-  get 'episodes/index'
-  get 'episodes/show'
-  get 'episodes/new'
-  get 'episodes/create'
-  get 'episodes/edit'
-  get 'episodes/destroy'
-  get 'shows/index'
-  get 'shows/show'
-  get 'shows/edit'
+  # get 'songs/index'
+  # get 'songs/show'
+  # get 'episodes/index'
+  # get 'episodes/show'
+  # get 'episodes/new'
+  # get 'episodes/create'
+  # get 'episodes/edit'
+  # get 'episodes/destroy'
+  # get 'shows/index'
+  # get 'shows/show'
+  # get 'shows/edit'
+ resources :episodes
+ resources :shows, only: [:edit, :index, :show]
+ resources :songs, only: [:index, :show]
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
