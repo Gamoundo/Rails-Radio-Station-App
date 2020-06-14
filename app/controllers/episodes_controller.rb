11a59be58421d8ce1ec4b94ace549ca948c75ade
Gamoundo <akilgg@gmail.com>
2020-06-13 21:23:26 -0400

    the start of the methods

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -8,14 +8,25 @@ class EpisodesController < ApplicationController
   end
 
   def new
+    @episode = Episode.new
+    @songs = Song.all
+    @shows = Show.all
   end
 
   def create
+   @episode.Episode.create(episode_params)
+   redirect_to episodes_path(@episode)
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def episode_params
+    params.require(:episode).permit(:name, :show_id, :song_id, :air_date)
   end
 end
