d424eb4b51b8f8d4e221daf2a52006545d2e8043
stephepush <7speter@gmail.com>
2020-06-13 20:51:25 -0400

    Adds shows & songs controllers + views

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,10 +1,13 @@
 class ShowsController < ApplicationController
   def index
+    @shows = Show.all
   end
 
   def show
+    @show = Show.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 end
